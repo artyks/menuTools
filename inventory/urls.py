65db2b1682e57787e4dfc125326ce6823582c668
elMolo <trevorrobert@gmail.com>
2021-07-16 18:28:43 -0400

    good urls and views for four main display pages, based on home template

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     # path("", views.home, name="home"),#function-based, for dev only. delete eventually
     path('', views.Home.as_view(), name='home'),#this path won't be used after dev. home.html is not a complete page. the actual home page destination is balancesheet.html
-    path("balancesheet", views.balancesheet, name="balancesheet"),#after all pages are running, this is the home page, served only after a successful login
+    path('balancesheet', views.BalanceSheet.as_view(), name='balancesheet'),
+    path('menu', views.Menu.as_view(), name='menu'),
+    path('inventory', views.Inventory.as_view(), name='inventory'),
+    path('purchases', views.Purchases.as_view(), name='purchases'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -4,13 +4,27 @@ from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+#import models
+from .models import MenuItem, Ingredient, Purchase, RecipeRequirement
 # Create your views here.
 # def home(request):
 #     return render(request, "inventory/home.html")
-class Home(TemplateView):
+class Home(TemplateView):#template view for now, later ListView
   template_name = "inventory/home.html"#soon this template gets replaced with balancesheet.html
 
+class BalanceSheet(TemplateView):#template view for now, later ListView
+  template_name = "inventory/balancesheet.html"
 
-    # return context
-def balancesheet(request):
-    return render(request, "inventory/balancesheet.html")
+class Inventory(TemplateView):#template view for now, later ListView
+  template_name = "inventory/inventory.html"
+
+class Menu(TemplateView):#template view for now, later ListView
+  template_name = "inventory/menu.html"
+
+class Purchases(TemplateView):#template view for now, later ListView
+  template_name = "inventory/purchases.html"
+
+
+
+# def balancesheet(request):
+    # return render(request, "inventory/balancesheet.html")
