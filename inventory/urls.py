b3c050e1053d8ddf64701e635db46eaa25a19a64
elMolo <trevorrobert@gmail.com>
2021-07-16 17:34:47 -0400

    first class based view and first url loading well

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("balancesheet", views.balancesheet, name="balancesheet"),
+    # path("", views.home, name="home"),#function-based, for dev only. delete eventually
+    path('', views.Home.as_view(), name='home'),#this path won't be used after dev. home.html is not a complete page. the actual home page destination is balancesheet.html
+    path("balancesheet", views.balancesheet, name="balancesheet"),#after all pages are running, this is the home page, served only after a successful login
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
 # Create your views here.
-def home(request):
-    return render(request, "inventory/home.html")
+# def home(request):
+#     return render(request, "inventory/home.html")
+class Home(TemplateView):
+  template_name = "inventory/home.html"#soon this template gets replaced with balancesheet.html
 
+
+    # return context
 def balancesheet(request):
     return render(request, "inventory/balancesheet.html")
